88b47621c56d89e8ff4ce07bce5f3d022ab1651f
KeeYoo <poronko2045@gmail.com>
2022-07-08 20:43:56 +0300

    Models are implemented

---
+++ OfficeEntity/OfficeEntity/Models/Project.cs
@@ -0,0 +1,11 @@
+﻿namespace OfficeEntity.Models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string Name { get; set; }
+        public decimal Budget { get; set; }
+        public DateTime StartedDate { get; set; }
+        public List<EmployeeProject> EmployeeProjects { get; set; } = new List<EmployeeProject>();
+    }
+}
---
+++ OfficeEntity/OfficeEntity/Models/Title.cs
@@ -0,0 +1,9 @@
+﻿namespace OfficeEntity.Models
+{
+    public class Title
+    {
+        public int TitleId { get; set; }
+        public string Name { get; set; }
+        public List<Employee> Employees { get; set; } = new List<Employee>();
+    }
+}
