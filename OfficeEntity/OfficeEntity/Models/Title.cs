628935151ba0a0945593453baab4f103c4424455
KeeYoo <73829550+kporonko@users.noreply.github.com>
2022-07-14 21:06:42 +0300

    Revert "Develop"

--- OfficeEntity/OfficeEntity/Models/Title.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OfficeEntity.Models
-{
-    public class Title
-    {
-        public int TitleId { get; set; }
-        public string Name { get; set; }
-        public virtual List<Employee> Employees { get; set; } = new List<Employee>();
-    }
-}
